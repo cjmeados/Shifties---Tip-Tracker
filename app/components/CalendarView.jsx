9aa3410582e0509f0a7978165a06b3aed669cac3
cjmeados <meadoschris@gmail.com>
2026-06-17 19:10:16 -0500

    adds dropdown for year selection

--- app/components/CalendarView.jsx
+++ app/components/CalendarView.jsx
@@ -3,15 +3,22 @@
 import { useState } from 'react'
 
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const years = []
 
 export default function CalendarView() {
 
     const today = new Date()
     const [currentMonth, setCurrentMonth] = useState(today.getMonth())
     const [currentYear, setCurrentYear] = useState(today.getFullYear())
+    const years = []
+    for (let i = today.getFullYear(); i >= 2020; i--) {
+        years.push(i)
+    }
 
     function nextMonth() {
-        if (currentMonth == 11) {
+        if (currentMonth == 11 && currentYear == today.getFullYear()) {
+            return
+        } else if (currentMonth == 11) {
             setCurrentMonth(0)
             setCurrentYear(currentYear + 1)
         } else {
@@ -28,13 +35,17 @@ export default function CalendarView() {
         }
     }
 
-
   return (
     <div>
         <div>
             <button onClick = {previousMonth}>Back</button>
             <h2>{monthNames[currentMonth]} {currentYear}</h2>
             <button onClick = {nextMonth}>Next</button>
+            <select value={currentYear} onChange={(e) => setCurrentYear(Number(e.target.value))}>
+                {years.map((year) => (
+                    <option value={year}>{year}</option>
+                ))}
+            </select>
         </div>
     </div>
   )
